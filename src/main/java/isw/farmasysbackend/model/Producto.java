6d1129d48587b2778082a8d26f932afddedaa05a
Brian La Torre <brianjairlatorre@gmail.com>
2026-06-14 15:39:26 -0500

    Configuración Inicial

---
+++ src/main/java/isw/farmasysbackend/model/Producto.java
@@ -0,0 +1,42 @@
+package isw.farmasysbackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "producto")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cod_barras", nullable = false, unique = true, length = 50)
+    private String codBarras;
+
+    @Column(name = "descripcion", nullable = false, length = 255)
+    private String descripcion;
+
+    @Column(name = "precio_venta", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioVenta;
+
+    @Column(name = "precio_costo", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioCosto;
+
+    @Column(name = "laboratorio", nullable = false, length = 120)
+    private String laboratorio;
+
+    @Column(name = "registro_sanitario", length = 60)
+    private String registroSanitario;
+
+    @Column(name = "condicion_venta", nullable = false, length = 30)
+    private String condicionVenta;
+}
